c29ad7cea9a75ebdd1a4af74aaf20c0da2ab786b
atia009 <aarontia009@gmail.com>
2023-02-19 20:04:41 -0800

    Add css styling

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import Img from "./image";
 import Form from "./form";
+import "./index.css";
+
+const header = document.createElement("h1");
+header.textContent = "Giphy Search";
+
+document.body.prepend(header);
