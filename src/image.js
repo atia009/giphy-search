86bc7c201bd4ab028f87812e569019f58fec0964
atia009 <aarontia009@gmail.com>
2023-03-03 10:26:12 -0800

    Refactor setImg function

--- src/image.js
+++ src/image.js
@@ -5,19 +5,18 @@ const Img = (() => {
   document.body.appendChild(img);
 
   // functions
-  function setImg(query) {
-    fetch(
-      `https://api.giphy.com/v1/gifs/translate?api_key=z4Q6aWFpoFeVBHp9mnmPPOmpENLtL8rG&s=${query}`,
-      { mode: "cors" }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        img.src = data.data.images.original.url;
-      })
-      .catch(() => {
-        img.src =
-          "https://media0.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif?cid=8b15c4243v1zf9cy7retvj9vlef329cmxcvl6e9aulkql30e&rid=giphy.gif&ct=g";
-      });
+  async function setImg(query) {
+    try {
+      const response = await fetch(
+        `https://api.giphy.com/v1/gifs/translate?api_key=z4Q6aWFpoFeVBHp9mnmPPOmpENLtL8rG&s=${query}`,
+        { mode: "cors" }
+      );
+      const data = await response.json();
+      img.src = data.data.images.original.url;
+    } catch (error) {
+      img.src =
+        "https://media0.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif?cid=8b15c4243v1zf9cy7retvj9vlef329cmxcvl6e9aulkql30e&rid=giphy.gif&ct=g";
+    }
   }
 
   // public methods
